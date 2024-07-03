38ac890b14ee51ada7f309976df3bf1a339f5eae
leticiappv <leticiappv@gmail.com>
2024-07-03 12:49:41 -0300

    Primeiro commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://miro.medium.com/v2/resize:fit:1358/1*5c8Khoy8ON4CwVqKKn6twA.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
